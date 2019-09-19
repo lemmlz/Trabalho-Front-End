500e1ad468fc6453ec6640f4f6dcdf41cfe2216e
lemmlz <a231005431@fumec.edu.br>
2019-09-19 16:00:59 -0300

    Maioria do projeto atual adaptado para angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './pages/header/header.component';
+import { FooterComponent } from './pages/footer/footer.component';
+import { SidebarLeftComponent } from './pages/sidebar-left/sidebar-left.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    SidebarLeftComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,

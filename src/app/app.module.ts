da5c53ba1549f87c5a8ced2fbd9474afca7314b1
lemmlz <a231005431@fumec.edu.br>
2019-09-28 19:55:13 -0300

    Criado a página de registrar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,7 @@ import { LaboratoryComponent } from 'src/app/pages/laboratories/laboratory/labor
 import { SidebarLeftService } from './services/sidebar-left.service';
 import { ComputersComponent } from './pages/computers/computers.component';
 import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
 
 @NgModule({
   declarations: [
@@ -23,7 +24,8 @@ import { LoginComponent } from './pages/login/login.component';
     LaboratoriesComponent,
     LaboratoryComponent,
     ComputersComponent,
-    LoginComponent
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,

c04cdc83591803246cff2c320b05868a9b98dacc
lemmlz <a231005431@fumec.edu.br>
2019-09-29 00:13:40 -0300

    Adicionado efeitos quando o foco está no text-box e criado booleanas para o controle das mensagens de validação

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -7,6 +7,52 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  //Validation
+  validationUser:boolean = false;
+  validationPassword:boolean = false;
+  validationLogin:boolean = false;
+
+  //FocusTextBox
+  isFocusUser:boolean = false;
+  isFocusPassword:boolean = false;
+
+  //Validation functions
+  focusUser(value:string){
+    this.isFocusUser = true;
+    if(value != ''){
+      this.validationUser = false;
+    } else {
+      if(this.validationLogin)
+        this.validationUser = true;
+    }
+  }
+
+  focusStopUser(value:string){
+    if(value === '')
+      this.isFocusUser = false;
+  }
+
+  focusPassword(value:string){
+    this.isFocusPassword = true;
+    if(value != ''){
+      this.validationPassword = false;
+    } else {
+      if(this.validationLogin)
+        this.validationPassword = true;
+    }
+  }
+
+  focusStopPassword(value:string){
+    if(value === '')
+      this.isFocusPassword = false;
+  }
+
+  dataValidation(){
+    this.validationLogin = true;
+    this.validationUser = true;
+    this.validationPassword = true;
+  }
+
   constructor() { }
 
   ngOnInit() {
